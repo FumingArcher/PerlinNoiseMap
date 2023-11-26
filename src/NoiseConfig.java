dca447e7bf8c28ae1faba1a67ed3dffffb0dca7c
FumingArcher <vvobag@gmail.com>
2023-11-26 13:22:21 +0400

    added navigation sex

--- src/NoiseConfig.java
+++ src/NoiseConfig.java
@@ -2,12 +2,15 @@ import java.awt.Color;
 import processing.core.PApplet;
 public class NoiseConfig extends PApplet {
 
-    float prvalue = 0.08F;
+    float prvalue = 0.1F;
+
+    float xoff = 0, yoff = 0;
+    float speed = 0.1F;
 
     public static int SCREEN_HEIGHT = 1280;
     public static int SCREEN_WIDTH = 720;
 
-    public static int UNIT_SIZE = 15;
+    public static int UNIT_SIZE = 5;
 
     public void settings(){
         size(SCREEN_WIDTH,SCREEN_HEIGHT);
@@ -34,10 +37,22 @@ public class NoiseConfig extends PApplet {
 
 
         }
+        if(key == 'w'){
+            yoff -= speed;
+        }
+        if(key =='s'){
+            yoff += speed;
+        }
+        if(key == 'a'){
+            xoff -= speed;
+        }
+        if(key == 'd'){
+            xoff += speed;
+        }
     }
 
     int GetTileType(int x,int y){
-        float t = noise(x*prvalue,y*prvalue);
+        float t = noise(xoff + x*prvalue,yoff + y*prvalue);
         if(t<0.3){
             //water
             return color(0,0,255);
@@ -60,6 +75,7 @@ public class NoiseConfig extends PApplet {
     public static void main(String[] args){
         PApplet.main("NoiseConfig");
     }
+
 
 
 }
