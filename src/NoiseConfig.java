a29a6fc381e8a5d019cf7709b221dc3609008a1f
FumingArcher <vvobag@gmail.com>
2024-01-09 22:05:26 +0400

    navigation

--- src/NoiseConfig.java
+++ src/NoiseConfig.java
@@ -1,16 +1,34 @@
 import java.awt.Color;
 import processing.core.PApplet;
+import processing.core.PImage;
+
 public class NoiseConfig extends PApplet {
 
     float prvalue = 0.08F;
 
+    float xoff = 0;
+
+    float yoff = 0;
+
+    float speed = 0.3F;
+
     public static int SCREEN_HEIGHT = 1280;
     public static int SCREEN_WIDTH = 720;
 
-    public static int UNIT_SIZE = 15;
+    public static int UNIT_SIZE = 5;
+
+    PImage[] sprites = new PImage[4];
+    public void loadImages() {
+
+        sprites[0] = loadImage("grass.png");
+        sprites[1] = loadImage("sand.png");
+        sprites[2] = loadImage("water.png");
+        sprites[3] = loadImage("hilly.png");
+    }
 
     public void settings(){
         size(SCREEN_WIDTH,SCREEN_HEIGHT);
+        loadImages();
     }
 
     public void draw(){
@@ -22,7 +40,7 @@ public class NoiseConfig extends PApplet {
             for(int j = 0;j<SCREEN_HEIGHT/UNIT_SIZE;j++){
                 int tileColor = GetTileType(i,j);
                 fill(tileColor);
-                rect(i*UNIT_SIZE, j*UNIT_SIZE, UNIT_SIZE,UNIT_SIZE);
+                image(sprites[GetTileType(i, j)], i * UNIT_SIZE, j * UNIT_SIZE, UNIT_SIZE, UNIT_SIZE);
             }
         }
     }
@@ -34,25 +52,37 @@ public class NoiseConfig extends PApplet {
 
 
         }
+        if(key == 'w'){
+            yoff -= speed;
+        }
+        if(key =='s'){
+            yoff += speed;
+        }
+        if(key == 'a'){
+            xoff -= speed;
+        }
+        if(key == 'd'){
+            xoff += speed;
+        }
     }
 
     int GetTileType(int x,int y){
-        float t = noise(x*prvalue,y*prvalue);
+        float t = noise(xoff + x*prvalue,yoff+ y*prvalue);
         if(t<0.3){
             //water
-            return color(0,0,255);
+            return 2;
         }
         else if(t>0.3 && t<0.4){
             //sand
-            return color(255, 255, 0);
+            return 1;
         }
         else if(t>0.4 && t<0.6){
             //grass
-            return color(0,245,0);
+            return 0;
         }
         else{
             //mountain
-            return color(128,128,128);
+            return 3;
         }
 
     }
